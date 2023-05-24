bfa1bb23d15563e048670711b9dd79988a5bba7a
Harish Rane <raneharish11@gmail.com>
2023-05-24 23:14:20 +0530

    File copying example

---
+++ FileCopyDemo.java
@@ -0,0 +1,48 @@
+import java.io.*;
+
+public class FileCopyDemo {
+
+    public static void main(String args[])throws Exception
+    {
+        File f1 = new File(args[0]);
+
+        if(!f1.exists())
+        {
+            System.out.println("File not present");
+        }
+        if(!f1.isFile())
+        {
+            System.out.println("It is not a file");
+        }
+        File f2 = new File(args[1]);
+
+        if(!f2.exists())
+        {
+            System.out.println("File not present");
+        }
+        if(!f2.isFile())
+        {
+            System.out.println("It is not a file");
+        }
+
+        FileReader fr = new FileReader(f1);
+        FileWriter fw = new FileWriter(f2);
+
+        fw.write("Author  : HARISH RANE 24/05/2023");
+
+        int ch =-1;
+
+        while(true)
+        {
+            ch=fr.read();
+            if(ch==-1)
+            break;
+
+            fw.write(ch);
+        }
+
+        fw.write("END-OF-FILE");
+        fr.close();
+        fw.close();
+    }
+}
